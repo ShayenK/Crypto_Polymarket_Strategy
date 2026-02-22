feac76426cbe243e8e1c17cf04b702db236fbac0
ShayenK <60591879+ShayenK@users.noreply.github.com>
2026-02-23 10:29:41 +1300

    trade evaluation system

---
+++ evaluation.py
@@ -0,0 +1,17 @@
+import requests
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+class ForwardTestEvaluation:
+    def __init__(self):
+        pass
+
+    def _retrieve_candle_data(self):
+        return
+    
+    def _load_prediction_logs(self):
+        return
+    
+    def _compare_to_actualised(self):
+        return
